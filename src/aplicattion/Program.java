0e4608c0c23c990cb5375712364bab7638ac7d0b
Herhos <herhos@outlook.com>
2023-02-07 13:57:54 -0300

    desafio_combate

---
+++ src/aplicattion/Program.java
@@ -0,0 +1,88 @@
+package aplicattion;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+import entities.Champion;
+
+public class Program
+{
+
+	public static void main(String[] args)
+	{
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n, i;		
+		
+		System.out.println("Digite os dados do primeiro campeão: ");
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		int vida = sc.nextInt();
+		System.out.print("Ataque: ");
+		int ataque = sc.nextInt();
+		System.out.print("Armadura: ");
+		int escudo = sc.nextInt();
+		Champion campeao1 = new Champion(nome, vida, ataque, escudo);
+		System.out.println();
+		
+		System.out.println("Digite os dados do segundo campeão: ");
+		System.out.print("Nome: ");
+		sc.nextLine();
+		nome = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		vida = sc.nextInt();
+		System.out.print("Ataque: ");
+		ataque = sc.nextInt();
+		System.out.print("Armadura: ");
+		escudo = sc.nextInt();
+		Champion campeao2 = new Champion(nome, vida, ataque, escudo);
+		
+		System.out.println();
+		System.out.print("Quantos turnos você deseja executar? ");
+		n = sc.nextInt();
+		System.out.println();
+		
+		for (i = 0; i < n; i++)
+		{
+			if (campeao1.getLife() <= 0 || campeao2.getLife() <= 0)
+			{
+				System.out.println("FIM DO COMBATE");
+				System.exit(0);
+			}
+			else
+			{				
+				campeao1.takeDamage(campeao2);
+				campeao2.takeDamage(campeao1);
+				
+				System.out.println("Resultado do turno " + (i + 1) +":"); 
+				
+				if (campeao1.getLife() < 0)
+				{
+					System.out.println(campeao1.getName() + ": " + " 0 de vida (morreu)");					
+				}
+				else
+				{
+					//System.out.println(campeao1.getName() + ": " + campeao1.getLife() + " de vida");
+					System.out.println(campeao1.status());					
+				}
+				if (campeao2.getLife() < 0)
+				{
+					System.out.println(campeao2.getName() + ": " + " 0 de vida (morreu)");					
+				}
+				else
+				{									
+					//System.out.println(campeao2.getName() + ": " + campeao2.getLife() + " de vida");
+					System.out.println(campeao2.status());
+				}				
+				System.out.println();
+			}			
+		}
+		
+		System.out.println("FIM DO COMBATE");
+		sc.close();
+	}
+}
---
+++ src/entities/Champion.java
@@ -0,0 +1,80 @@
+package entities;
+
+public class Champion
+{
+	private String name;
+	private int life;
+	private int attack;
+	private int armor;
+	
+	public Champion() {}
+	
+	public Champion(String name, int life, int attack, int armor)
+	{
+		this.name = name;
+		this.life = life;
+		this.attack = attack;
+		this.armor = armor;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getLife()
+	{
+		return life;
+	}
+
+	public void setLife(int life)
+	{
+		this.life = life;
+	}
+
+	public int getAttack()
+	{
+		return attack;
+	}
+
+	public void setAttack(int attack)
+	{
+		this.attack = attack;
+	}
+
+	public int getArmor()
+	{
+		return armor;
+	}
+
+	public void setArmor(int armor)
+	{
+		this.armor = armor;
+	}	
+	
+	public void takeDamage(Champion other)
+	{		
+		if (this.attack < other.armor)
+		{
+			this.life = this.life - (Math.abs(other.attack - this.armor));
+			other.life++;
+		}
+		else
+		{
+			this.life = this.life - (Math.abs(other.attack - this.armor));
+		}		
+	}
+	
+	public String status()
+	{
+		return name
+				+ ": "
+				+ life
+				+ " de vida";
+	}
+}
